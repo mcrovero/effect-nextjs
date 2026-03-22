cb465f15879117e358f2e02ccea829553475ecc9
Mattia Crovero <mcrovero@gmail.com>
2026-03-22 11:30:21 +0100

    fix: remove workaround asynclocalstorage

--- src/Cache.ts
+++ src/Cache.ts
@@ -2,9 +2,7 @@
  * @since 0.30.0
  */
 import { Effect } from "effect"
-import * as Context_ from "effect/Context"
 import { revalidatePath, revalidateTag } from "next/cache.js"
-import { ContextWrapperService } from "./internal/async-context.js"
 
 /**
  * Revalidate a specific path.
@@ -14,15 +12,7 @@ import { ContextWrapperService } from "./internal/async-context.js"
  */
 export const RevalidatePath = (
   ...args: Parameters<typeof revalidatePath>
-): Effect.Effect<void, never, never> =>
-  Effect.flatMap(
-    Effect.context<never>(),
-    (context) => {
-      const wrapWithContext = Context_.unsafeGet(context, ContextWrapperService)
-      const wrappedFn = wrapWithContext(revalidatePath)
-      return Effect.sync(() => wrappedFn(...args))
-    }
-  )
+): Effect.Effect<void, never, never> => Effect.sync(() => revalidatePath(...args))
 
 /**
  * Revalidate a cache tag.
@@ -32,12 +22,4 @@ export const RevalidatePath = (
  */
 export const RevalidateTag = (
   ...args: Parameters<typeof revalidateTag>
-): Effect.Effect<void, never, never> =>
-  Effect.flatMap(
-    Effect.context<never>(),
-    (context) => {
-      const wrapWithContext = Context_.unsafeGet(context, ContextWrapperService)
-      const wrappedFn = wrapWithContext(revalidateTag)
-      return Effect.sync(() => wrappedFn(...args))
-    }
-  )
+): Effect.Effect<void, never, never> => Effect.sync(() => revalidateTag(...args))
